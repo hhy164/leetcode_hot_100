fac142b1d73a26dbd7e6a90cf49578c3df0321bf
何红妤#A05018 <hongyu.he@casstime.com>
2025-10-14 11:39:20 +0800

    feat: 53课程表

--- 53课程表.js
+++ 53课程表.js
@@ -1,5 +1,43 @@
+/**
+ * 利用拓扑排序解题
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
 var canFinish = function (numCourses, prerequisites) {
-
+  const indgree = Array.from({ length: numCourses }, () => 0);
+  const map = new Map();
+  for (const [a, b] of prerequisites) {
+    if (map.has(b)) {
+      map.get(b).push(a)
+    } else {
+      map.set(b, [a])
+    }
+    indgree[a]++;
+  }
+  const queue = []
+  for (let i = 0; i < numCourses; i++) {
+    if (indgree[i] === 0) {
+      // 入度为0的节点
+      queue.push(i)
+    }
+  }
+  let result = [];
+  while (queue.length) {
+    const cur = queue.shift();
+    result.push(cur);
+    const nodes = map.get(cur);
+    if (!nodes) continue;
+    for (const item of nodes) {
+      indgree[item]--;
+      if (indgree[item] === 0) {
+        queue.push(item);
+      }
+    }
+  }
+  return result.length === numCourses;
 };
 
-canFinish(2, [[1, 0]])
+const numCourses = 2, prerequisites = [[1, 0], [0, 1]]
+const result = canFinish(numCourses, prerequisites)
+console.log(result)
