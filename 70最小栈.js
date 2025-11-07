13e5597251fbf72f58ddc91d544142c71ea61e33
何红妤#A05018 <hongyu.he@casstime.com>
2025-11-07 16:57:54 +0800

    feat: 70最小栈[https://jira.casstime.com/EC-000]

--- 70最小栈.js
+++ 70最小栈.js
@@ -1,39 +1,24 @@
 class MinStack {
   constructor() {
     this.value = [];
+    this.minStack = [];
   }
   push(val) {
     this.value.push(val);
-    this.min = Math.min(this.min, val)
+    if (!this.minStack.length || val <= this.minStack[this.minStack.length - 1]) {
+      this.minStack.push(val)
+    }
   }
   pop() {
-    return this.value.pop();
+    const item = this.value.pop();
+    if (item === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop()
+    }
   }
   top() {
-    return this.value[this.value.length - 1]
+    return this.value[this.value.length - 1];
   }
   getMin() {
-    let min = Infinity;
-    for (let i = 0; i < this.value.length; i++) {
-      min = Math.min(min, this.value[i]);
-    }
-    return min
+    return this.minStack[this.minStack.length - 1]
   }
 }
-const obj = new MinStack()
-obj.push(-2);
-obj.push(0);
-obj.push(-3);
-console.log(obj.getMin())
-console.log(obj.pop());
-console.log(obj.top());
-console.log(obj.getMin())
-
-/** 
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
