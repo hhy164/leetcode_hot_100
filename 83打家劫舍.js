afeaef81afdfccbb47375a0b88a7c36fd158b74e
何红妤#A05018 <hongyu.he@casstime.com>
2025-11-11 11:43:28 +0800

    feat: 打家劫舍

---
+++ 83打家劫舍.js
@@ -0,0 +1,10 @@
+var rob = function (nums) {
+  const dp = [nums[0], Math.max(nums[0], nums[1])];
+  for (let i = 2; i < nums.length; i++) {
+    dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+  }
+  return dp[nums.length - 1]
+}
+const nums = [2, 7, 9, 3, 1];
+const result = rob(nums);
+console.log(result);
